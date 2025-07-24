5d545cb35d62b4cf6ce234be20ecca1b9be88a97
Hex Vai <bdsajibkumar0@gmail.com>
2025-07-24 20:23:33 +0600

    Update sok.py

--- sok.py
+++ sok.py
@@ -1,21 +1,21 @@
 import subprocess
 
-HLS_URL = "https://tvsen6.aynaott.com/tsportsfhd/tracks-v1a1/mono.ts.m3u8"  # replace with your real .m3u8
-RTMP_URL = "rtmp://live.restream.io/live/re_7638211_event4c220a8725e842108313d235ea41e326"  # replace with your actual RTMP
+HLS_URL = "https://tvsen6.aynaott.com/tsportsfhd/tracks-v1a1/mono.ts.m3u8"
+RTMP_URL = "rtmp://live.restream.io/live/re_7638211_event4c220a8725e842108313d235ea41e326"
 
 def stream_to_rtmp():
-   command = [
-    "ffmpeg",
-    "-re",
-    "-i", HLS_URL,
-    "-c:v", "libx264",
-    "-preset", "veryfast",
-    "-b:v", "2000k",
-    "-c:a", "aac",
-    "-b:a", "128k",
-    "-f", "flv",
-    RTMP_URL
-]
+    command = [
+        "ffmpeg",
+        "-re",
+        "-i", HLS_URL,
+        "-c:v", "libx264",
+        "-preset", "veryfast",
+        "-b:v", "2000k",
+        "-c:a", "aac",
+        "-b:a", "128k",
+        "-f", "flv",
+        RTMP_URL
+    ]
 
     print("▶️ Starting FFmpeg stream...")
     result = subprocess.run(command, capture_output=True, text=True)
